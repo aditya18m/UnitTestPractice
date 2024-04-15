cebbb0e3dad3dd416f4d14d2b431cfc5b4b454dd
Aditya Mohandas <amohandas@csuchico.edu>
2024-04-15 11:53:15 -0700

    added unit tests

--- Password.cpp
+++ Password.cpp
@@ -9,12 +9,15 @@ using std::string;
   end of the string). The function is case-sensitive so 'Z' is different than
   'z' and any ASCII characters are allowed.
 */
-int Password::count_leading_characters(string phrase){
-  int repetition = 1;
-  int index = 0;
-  while( index < phrase.length()-1 && phrase[index] == phrase[index+1] ){
-    repetition++;
-    index++;
-  }
-  return repetition;
+int Password::count_leading_characters(string phrase) {
+    if (phrase.empty()) {
+        return 0;
+    }
+    int repetition = 1;
+    int index = 0;
+    while (index < phrase.length()-1 && phrase[index] == phrase[index+1]) {
+        repetition++;
+        index++;
+    }
+    return repetition;
 }
--- PasswordTest.cpp
+++ PasswordTest.cpp
@@ -20,3 +20,26 @@ TEST(PasswordTest, single_letter_password)
 	int actual = my_password.count_leading_characters("Z");
 	ASSERT_EQ(1, actual);
 }
+TEST_F(PracticeTest, empty_password) {
+    Password my_password;
+    int actual = my_password.count_leading_characters("");
+    ASSERT_EQ(0, actual);
+}
+
+TEST_F(PracticeTest, single_character_repeated) {
+    Password my_password;
+    int actual = my_password.count_leading_characters("aaaaa");
+    ASSERT_EQ(5, actual);
+}
+
+TEST_F(PracticeTest, no_repeats) {
+    Password my_password;
+    int actual = my_password.count_leading_characters("abcdef");
+    ASSERT_EQ(1, actual);
+}
+
+TEST_F(PracticeTest, case_sensitivity) {
+    Password my_password;
+    int actual = my_password.count_leading_characters("aAa");
+    ASSERT_EQ(1, actual);
+}
