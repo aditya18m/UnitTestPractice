28b26c44f1f372606cc3ee3c1eb702c6bf9da889
Aditya Mohandas <amohandas@csuchico.edu>
2024-04-24 11:38:26 -0700

    Added additional tests

--- Password.cpp
+++ Password.cpp
@@ -1,8 +1,16 @@
 #include "Password.h"
 #include <string>
+#include <algorithm>
+#include <cctype>
+#include <vector>
 
 using std::string;
+using std::vector;
 
+
+Password::Password() {
+    pass_history.push_back("ChicoCA-95929");
+}
 /*
   The function receives a string counts how many times the same character 
   occurs at the beginning of the string, before any other characters (or the
@@ -20,4 +28,111 @@ int Password::count_leading_characters(string phrase) {
         index++;
     }
     return repetition;
+}
+
+// Implementation A
+// #include <cctype>
+// bool Password::has_mixed_case(string str)
+// {
+//   bool has_lower = false;
+//   bool has_upper = false;
+//   for (char ch : str)
+//   {
+//     if (std::islower(ch))
+//     {
+//       has_lower = true;
+//     }
+//     else if (std::isupper(ch))
+//     {
+//       has_upper = true;
+//     }
+//   }
+//   return has_lower && has_upper;
+// }
+// Implementation B
+// bool Password::has_mixed_case(string str)
+// {
+//   int count_lower = 0;
+//   for (int i = 0; i < str.length(); i++)
+//   {
+//     if (str[i] >= 'a' && str[i] <= 'z')
+//     {
+//       count_lower++;
+//     }
+//   }
+//   return count_lower > 0 && count_lower < str.length();
+// }
+// Implementation C
+// bool Password::has_mixed_case(string str)
+// {
+//   bool lower = false;
+//   bool upper = false;
+//   for (int i = 0; i < str.length(); i++)
+//   {
+//     if (str[i] >= 'a' && str[i] <= 'z')
+//     {
+//       lower = true;
+//     }
+//     else if (str[i] >= 'A' && str[i] <= 'Z')
+//     {
+//       upper = true;
+//     }
+//   }
+//   return upper && lower;
+// }
+// Implementation D
+// #include <cctype>
+// bool Password::has_mixed_case(string str)
+// {
+//   bool is_lower = false;
+//   bool is_upper = false;
+//   for (char c : str)
+//   {
+//     if (std::islower(c))
+//     {
+//       is_lower = true;
+//     }
+//     else
+//     {
+//       is_upper = true;
+//     }
+//   }
+//   return is_lower && is_upper;
+// }
+// Implementation E
+bool Password::has_mixed_case(string str)
+{
+  bool found = false;
+  for(char c : str){
+    if( !found && c >= 'A' && c <= 'Z' ){
+      found = true;
+    }
+    else if( found && c >= 'a' && c <= 'z'){
+      return true;
+    }
+  }
+  return false;
+}
+
+void Password::set(string new_password) {
+    if (new_password.length() < 8 || new_password.length() > 20)
+        return;
+  
+    if (count_leading_characters(new_password) > 3)
+        return;
+  
+    if (!has_mixed_case(new_password))
+        return;
+  
+    if (std::find(pass_history.begin(), pass_history.end(), new_password) != pass_history.end())
+        return;
+  
+    pass_history.push_back(new_password);
+}
+
+bool Password::authenticate(string test_password) {
+    if (pass_history.empty())
+        return false;
+  
+    return test_password == pass_history.back();
 }
--- PasswordTest.cpp
+++ PasswordTest.cpp
@@ -5,6 +5,18 @@
 #include <gtest/gtest.h>
 #include "Password.h"
 
+
+class PasswordTest : public ::testing::Test {
+protected:
+    Password my_password; 
+    virtual void SetUp() {
+        my_password = Password();  
+    }
+    virtual void TearDown() {
+    }
+};
+
+
 class PracticeTest : public ::testing::Test
 {
 	protected:
@@ -13,6 +25,8 @@ class PracticeTest : public ::testing::Test
 		virtual void SetUp(){} //sets up before each test (after constructor)
 		virtual void TearDown(){} //clean up after each test, (before destructor)
 };
+
+
 
 TEST(PasswordTest, single_letter_password)
 {
@@ -42,4 +56,54 @@ TEST_F(PracticeTest, case_sensitivity) {
     Password my_password;
     int actual = my_password.count_leading_characters("aAa");
     ASSERT_EQ(1, actual);
+}
+
+TEST_F(PasswordTest, MixedCaseDetection) {
+    ASSERT_FALSE(my_password.has_mixed_case("aaaaa"));
+    ASSERT_FALSE(my_password.has_mixed_case("AAAAA"));
+    ASSERT_TRUE(my_password.has_mixed_case("aA"));
+    ASSERT_TRUE(my_password.has_mixed_case("Aa"));
+    ASSERT_FALSE(my_password.has_mixed_case("12345"));
+    ASSERT_FALSE(my_password.has_mixed_case("!!!!"));
+}
+TEST_F(PasswordTest, SetPasswordCriteria) {
+    my_password.set("Short1");
+    ASSERT_FALSE(my_password.authenticate("Short1"));
+    my_password.set("ThisIsAVeryLongPasswordExceedingLimit");
+    ASSERT_FALSE(my_password.authenticate("ThisIsAVeryLongPasswordExceedingLimit"));
+    my_password.set("alllowercase");
+    ASSERT_FALSE(my_password.authenticate("alllowercase"));
+    my_password.set("ValidPass1");
+    ASSERT_TRUE(my_password.authenticate("ValidPass1"));
+    my_password.set("aaaaValidPass1");
+    ASSERT_FALSE(my_password.authenticate("aaaaValidPass1"));
+}
+
+TEST_F(PasswordTest, PasswordHistory) {
+    my_password.set("Password1");
+    my_password.set("Password2");
+    my_password.authenticate("Password1");
+    ASSERT_FALSE(my_password.authenticate("Password1"));
+    ASSERT_TRUE(my_password.authenticate("Password2"));
+}
+
+TEST_F(PasswordTest, RepeatPassword) {
+    my_password.set("RepeatPass1");
+    ASSERT_TRUE(my_password.authenticate("RepeatPass1"));
+    my_password.set("RepeatPass1");  
+    my_password.set("NewPass1");  
+    ASSERT_FALSE(my_password.authenticate("RepeatPass1"));
+    ASSERT_TRUE(my_password.authenticate("NewPass1"));
+}
+
+TEST_F(PasswordTest, DefaultPasswordSet) {
+    ASSERT_TRUE(my_password.authenticate("ChicoCA-95929"));
+}
+
+TEST_F(PasswordTest, CountLeadingCharacters) {
+    ASSERT_EQ(1, my_password.count_leading_characters("Z"));
+    ASSERT_EQ(0, my_password.count_leading_characters(""));
+    ASSERT_EQ(5, my_password.count_leading_characters("aaaaa"));
+    ASSERT_EQ(1, my_password.count_leading_characters("abcdef"));
+    ASSERT_EQ(1, my_password.count_leading_characters("aAa"));
 }
